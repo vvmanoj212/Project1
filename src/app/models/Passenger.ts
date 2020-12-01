9611445c7520dac11e62923e946ceea2b2521b26
VaibhavGangwar-Code <72292120+VaibhavGangwar-Code@users.noreply.github.com>
2020-12-01 08:30:07 +0530

    Added comments

--- src/app/models/Passenger.ts
+++ src/app/models/Passenger.ts
@@ -2,6 +2,8 @@ export class Passenger{
     title:string;
     firstname:string;
     lastname:string;
+  
+  //Declaring the variables for class
     constructor(Title,Fname,Lname){
         this.title=Title;
         this.firstname=Fname;
